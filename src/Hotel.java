f19a849a1b4c16998791e410ac02bee1606388d6
TIME SHIFTER <asor01810@gmail.com>
2026-04-02 14:32:41 +0300

    Update Hotel.java

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,8 +1,11 @@
 public class Hotel {
+
+// Attributes
 	private Room [] rooms;
 	private String name;
 	private int count;
-	
+
+	// Constructor
 	public Hotel(String name, int size) {
 		
 		setName(name);
@@ -11,6 +14,7 @@ public class Hotel {
 		
 	}
 	
+// Method for adding guest to room
 	public boolean addReservation(int num, int days, Guest guest, String type) {
 		
 		if (count < rooms.length) {
@@ -27,6 +31,7 @@ public class Hotel {
 		
 	}
 	
+// finding room using recursion
 	public int findRoom(int RoomNumber, int index) {
 		
 		if (index >= count) return -1;
@@ -36,6 +41,7 @@ public class Hotel {
 		return findRoom(RoomNumber, index + 1);
 	}
 	
+// end current reservation
 	public boolean removeReservation(int RoomNumber) {
 		
 		int i = this.findRoom(RoomNumber, 0);
@@ -52,8 +58,11 @@ public class Hotel {
 		
 	}
 	
-	public double getTotalRevnue(int RoomNumber, int index) {
-		if(index >= count) return 0;
+// gets total payment for specific room using it's number
+	public double getTotalRevnue(int 
+  RoomNumber, int index) {
+		if(index >= count)
+    return 0;
 		if(rooms[index].getRoomnumber() == RoomNumber) {
 			return rooms[index].calculatePrice();		
 		}
@@ -64,6 +73,7 @@ public class Hotel {
 		return rooms[i];
 	}
 	
+// Setters and Getters
 	public String getName() {
 		return name;
 	}
