bd8123dbbf97d0a63c69673643eea41c05a1fcb2
TIME SHIFTER <asor01810@gmail.com>
2026-04-02 15:12:23 +0300

    Update StandardRoom.java

--- src/StandardRoom.java
+++ src/StandardRoom.java
@@ -1,15 +1,19 @@
-public class StandardRoom extends Room {
+public class StandardRoom extends Room { // inherent from class Room
 	
+// Attributes
 	private double pricePNight = 100;
 	
+// Constructor
 	public StandardRoom(int num, int days, Guest guest) {
 		super(num,days,guest);
 	}
-	
+
+	// Overriding Polymorphic method
 	public double calculatePrice() {
 		return (days*pricePNight);
 	}
 	
+// Setters & Getters
 	public void setPricePNight(double price) {
 		pricePNight = price;
 	}
