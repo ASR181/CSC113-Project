c9918855154cbc10c68f52846a9f8fadb50029f2
TIME SHIFTER <asor01810@gmail.com>
2026-04-02 15:24:26 +0300

    Update Suite.java

--- src/Suite.java
+++ src/Suite.java
@@ -1,16 +1,20 @@
-public class Suite extends Room {
+public class Suite extends Room { // inherent from class Room
 	
+// Attributes
 	protected double pricePNight = 250;
-	
+	protected double VIPService = 500;
+
+// Constructor
 	public Suite(int num, int days, Guest guest) {
 		super(num,days,guest);
 	}
 	
+// Overriding Polymorphic method
 	public double calculatePrice() {
-		return (days*pricePNight);
+		return (days*pricePNight + VIPService);
 	}
 	
-
+// Setters and Getters
 	public void setPricePNight(double price) {
 		pricePNight = price;
 	}
