b073a7f375c25af0f6c41898441bb7421ca166ba
TIME SHIFTER <asor01810@gmail.com>
2026-04-02 15:08:10 +0300

    Update PentHouse.java

--- src/PentHouse.java
+++ src/PentHouse.java
@@ -1,11 +1,14 @@
-public class PentHouse extends Suite {
+public class PentHouse extends Suite { // inherent from class Room
 	
+// Attributes
 	protected double pricePNight = 500;
 	
+// Constructor
 	public PentHouse(int num, int days, Guest guest) {
 		super(num, days, guest);
 	}
 	
+// Overriding Polymorphic method
 	public double calculatePrice() {
 		return (days*pricePNight);
 	}
